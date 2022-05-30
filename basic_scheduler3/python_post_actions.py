75fa218a238710bb88752c529fb8ebc667f9b451
axlan <feros32@gmail.com>
2022-05-30 13:01:41 -0700

    Run python script on completion.

--- basic_scheduler3/python_post_actions.py
+++ basic_scheduler3/python_post_actions.py
@@ -3,11 +3,13 @@ import subprocess
 
 tool_path = "C:/Program Files (x86)/Atmel/Studio/7.0/toolchain/avr8/avr8-gnu-toolchain/bin/"
 
-task_object_file = "task3/Debug/main.o"
+project_dir = "C:/Users/feros/Documents/src/basic_scheduler/"
 
-task_main_dump = "basic_scheduler3/Debug/task3.bin"
+task_object_file = project_dir + "task3/Debug/main.o"
 
-scheduler_elf_file = "basic_scheduler3/Debug/basic_scheduler3.elf"
+task_main_dump = project_dir + "basic_scheduler3/Debug/task3.bin"
+
+scheduler_elf_file = project_dir + "basic_scheduler3/Debug/basic_scheduler3.elf"
 
 
 subprocess.run([tool_path + 'avr-objcopy', "-O", "binary", "--only-section=.text.*", task_object_file, task_main_dump])
